c061cbf4a376014504a3d44dc6a2ab5853d9deb6
unknown <felipecod6@hotmail.com>
2018-12-25 23:46:54 -0200

    adionando o page loader

--- js/app.js
+++ js/app.js
@@ -35,3 +35,8 @@ var reduceNavbar = function() {
 reduceNavbar();
 
 debounce($(window).scroll(reduceNavbar),300);
+
+jQuery(window).load(function () {
+	$(".loader").delay(1500).fadeOut("slow"); //retire o delay quando for copiar!
+  $("#tudo_page").toggle("fast");
+});
